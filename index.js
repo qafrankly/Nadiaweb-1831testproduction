033bb11915232f152a3e3fb7b0dba1e7987a3d2d
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-12-13 15:35:58 -0500

    adding lifecycle logging

--- index.js
+++ index.js
@@ -2,7 +2,29 @@ import React, { Component, PropTypes } from 'react';
 import Image from 'components/Image';
 
 class ModuleDemo extends Component {
-
+   componentWillMount() {
+      console.log('Component WILL MOUNT!')
+   }
+   componentDidMount() {
+      console.log('Component DID MOUNT!')
+   }
+   componentWillReceiveProps(newProps) {    
+      console.log('Component WILL RECIEVE PROPS!')
+   }
+   shouldComponentUpdate(newProps, newState) {
+      console.log('shouldComponentUpdate!')
+      return true;
+   }
+   componentWillUpdate(nextProps, nextState) {
+      console.log('Component WILL UPDATE!');
+   }
+   componentDidUpdate(prevProps, prevState) {
+      console.log('Component DID UPDATE!')
+   }
+   componentWillUnmount() {
+      console.log('Component WILL UNMOUNT!')
+   }
+	
   render(){
 	  
     return (
