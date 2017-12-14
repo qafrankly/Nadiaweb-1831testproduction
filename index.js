b6c1e6b51cae788539d29ead95f7c6bef20afdef
qafrankly <31970630+qafrankly@users.noreply.github.com>
2017-12-14 10:20:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
+
 import React, { Component, PropTypes } from 'react';
- //import Image from 'components/Image';
+import Image from 'components/Image';
 
 class ModuleDemo extends Component {
-   
- componentWillMount() {
-      console.log('Component WILL MOUNT!')
-   }
-   componentDidMount() {
-      console.log('Component DID MOUNT!')
-   }
-   componentWillReceiveProps(newProps) {    
-      console.log('Component WILL RECIEVE PROPS!')
-   }
-   shouldComponentUpdate(newProps, newState) {
-      console.log('shouldComponentUpdate!')
-      return true;
-   }
-   componentWillUpdate(nextProps, nextState) {
-      console.log('Component WILL UPDATE!');
-   }
-   componentDidUpdate(prevProps, prevState) {
-      console.log('Component DID UPDATE!')
-   }
-   componentWillUnmount() {
-      console.log('Component WILL UNMOUNT!')
-   }
 
-	
   render(){
 	  
     return (
       <div className='FranklyModulesDemo'>
 	<h2 className='demo_text'>{this.props.text}</h2>
-	<p>This text is not editable!!</p>
+	<p>This text is not editable!</p>
 	<Image width={280} src={'http://franklydemo.images.worldnow.com/images/15208935_G.jpeg'} alt={'alt'} />
       </div>
     );
